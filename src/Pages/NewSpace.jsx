10a2c6ace490e7326d5e5352440e16030212a319
Rahul Malhotra <rahulmalhotra236@gmail.com>
2024-07-10 22:08:48 +0530

    add tailwind to other pages

--- src/Pages/NewSpace.jsx
+++ src/Pages/NewSpace.jsx
@@ -37,75 +37,94 @@ const NewSpace = () => {
 
 
   return (
-    <form
-      action={`/space-created/${formData.spaceName}`}
-      onSubmit={handleSpaceData}
-      method="post"
-    >
-      <div className="max-w-md mx-auto bg-white p-8 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          Create a New Space
-        </h2>
-
-        <div className="mb-4">
-          <label
-            htmlFor="space-name"
-            className="block text-gray-700 font-medium mb-2"
-          >
-            Space Name
-          </label>
-          <input
-            required
-            type="text"
-            id="space-name"
-            name="spaceName"
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-          />
+    <div className=" mx-36 my-10 flex gap-16 shadow-2xl justify-center rounded-lg py-10 px-8">
+      <div className="w-1/2 border rounded-lg p-10 flex flex-col justify-center gap-20 items-center">
+        <div className="flex flex-col items-center gap-5">
+          <h3 className="text-gray-700 font-bold text-4xl">
+            Header goes here...
+          </h3>
+          <p className="text-gray-400">Your custom message goes here...</p>
         </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="header-title"
-            className="block text-gray-700 font-medium mb-2"
-          >
-            Header Title
-          </label>
-          <input
-            required
-            type="text"
-            id="header-title"
-            name="headerTitle"
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-          />
+        <div>
+          <butto className="bg-[#5C5CFC] text-white px-6 py-4 w-full  cursor-pointer">
+            Send in text
+          </butto>
         </div>
-
-        <div className="mb-6">
-          <label
-            htmlFor="your-custom-message"
-            className="block text-gray-700 font-medium mb-2"
-          >
-            Your Custom Message
-          </label>
-          <input
-            required
-            type="text"
-            id="your-custom-message"
-            name="yourCustomMessage"
-            onChange={handleInputChange}
-            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-          />
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
-        >
-          Create Space
-        </button>
       </div>
-    </form>
+      <form
+        action={`/space-created/${formData.spaceName}`}
+        onSubmit={handleSpaceData}
+        method="post"
+      >
+        <div className=" mx-auto bg-white  rounded-lg flex flex-col justify-center items-center gap-4 p-6">
+          <h2 className="text-4xl font-bold mb-6 text-center">
+            Create a New Space
+          </h2>
+          <p className="text-slate-500 ">
+            After the Space is created, it will generate a dedicated page for
+            collecting testimonials.
+          </p>
+
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="space-name"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Space Name
+            </label>
+            <input
+              required
+              type="text"
+              id="space-name"
+              name="spaceName"
+              onChange={handleInputChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+            />
+          </div>
+
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="header-title"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Header Title
+            </label>
+            <input
+              required
+              type="text"
+              id="header-title"
+              name="headerTitle"
+              onChange={handleInputChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+            />
+          </div>
+
+          <div className="mb-6 w-full">
+            <label
+              htmlFor="your-custom-message"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Your Custom Message
+            </label>
+            <input
+              required
+              type="text"
+              id="your-custom-message"
+              name="yourCustomMessage"
+              onChange={handleInputChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300"
+          >
+            Create Space
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- src/Pages/ShowSpaceData.jsx
+++ src/Pages/ShowSpaceData.jsx
@@ -25,17 +25,64 @@ const ShowSpaceData = () => {
     <div className="flex flex-col  gap-20 min-h-screen bg-[#151719] px-20 py-5">
       <Navbar />
 
-      {data ? (
-        data.map((d, index) => (
-          <div key={index} className="mb-4 p-4 border rounded shadow">
-            <h1 className="text-xl font-bold mb-2">{d.yourName}</h1>
-            <p className="text-gray-700">{d.yourTestimonial}</p>
-            <p className="text-gray-700">{d.yourEmail}</p>
+      <div>
+        {data.map((d, index) => (
+          <div key={index} className="flex justify-between items-center">
+            <div>
+              <h1 className="text-white text-4xl font-bold mb-2">
+                {d.yourName}
+              </h1>
+              <p className="text-white text-gray-400">
+                Space public URL:
+                <a href="#" className="underline">
+                  LINK
+                </a>
+              </p>
+            </div>
+
+            <div>
+              <button className="bg-white rounded-md px-5 py-2">
+                Edit Space
+              </button>
+            </div>
           </div>
-        ))
-      ) : (
-        <p className="text-gray-500">No testimonials found.</p>
-      )}
+        ))}
+      </div>
+
+      <div className="flex w-full  p-6">
+        <div className="w-1/2 cursor-pointer text-xl font-semibold text-white ">
+          Wall of Love
+        </div>
+        <div className="w-full bg-[#292C30] rounded-lg text-gray-400">
+          {data ? (
+            data.map((d, index) => (
+              <div key={index} className="mb-4 p-10 shadow">
+                <p className="text-gray-200">{d.yourTestimonial}</p>
+                <br />
+                <div className="flex md:justify-between w-1/2">
+                  <div>
+                    <p>Name</p>
+                    <p >{d.yourName}</p>
+                  </div>
+                  <div>
+                    <p>Email</p>
+                    <p>{d.yourEmail}</p>
+                  </div>
+                  
+                </div>
+                <br />
+                <div>
+                  <p>Submitted at</p>
+                  <p>1/2/2002</p>
+                </div>
+
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-500">No testimonials found.</p>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
