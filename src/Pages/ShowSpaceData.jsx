e947990ba6ae76f78b68665d5c35ec28e3f947d4
Rahul Malhotra <rahulmalhotra236@gmail.com>
2024-07-11 11:38:06 +0530

    update show single space testimonails in frontend

--- src/Pages/ShowSpaceData.jsx
+++ src/Pages/ShowSpaceData.jsx
@@ -26,56 +26,50 @@ const ShowSpaceData = () => {
       <Navbar />
 
       <div>
-        {data.map((d, index) => (
-          <div key={index} className="flex justify-between items-center">
-            <div>
-              <h1 className="text-white text-4xl font-bold mb-2">
-                {d.yourName}
-              </h1>
-              <p className="text-white text-gray-400">
-                Space public URL:
-                <a href="#" className="underline">
-                  LINK
-                </a>
-              </p>
-            </div>
-
-            <div>
-              <button className="bg-white rounded-md px-5 py-2">
-                Edit Space
-              </button>
-            </div>
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-white text-4xl font-bold mb-2">{spaceName}</h1>
+            <p className="text-white text-gray-400">
+              Space public URL:
+              <a href="#" className="underline">
+                LINK
+              </a>
+            </p>
           </div>
-        ))}
+
+          <div>
+            <button className="bg-white rounded-md px-5 py-2">
+              Edit Space
+            </button>
+          </div>
+        </div>
       </div>
 
       <div className="flex w-full  p-6">
         <div className="w-1/2 cursor-pointer text-xl font-semibold text-white ">
           Wall of Love
         </div>
-        <div className="w-full bg-[#292C30] rounded-lg text-gray-400">
+        <div className="w-full  flex md:flex-col gap-10   text-gray-400">
           {data ? (
             data.map((d, index) => (
-              <div key={index} className="mb-4 p-10 shadow">
+              <div key={index} className=" p-10 shadow rounded-lg bg-[#292C30]">
                 <p className="text-gray-200">{d.yourTestimonial}</p>
                 <br />
                 <div className="flex md:justify-between w-1/2">
                   <div>
                     <p>Name</p>
-                    <p >{d.yourName}</p>
+                    <p>{d.yourName}</p>
                   </div>
                   <div>
                     <p>Email</p>
                     <p>{d.yourEmail}</p>
                   </div>
-                  
                 </div>
                 <br />
                 <div>
                   <p>Submitted at</p>
                   <p>1/2/2002</p>
                 </div>
-
               </div>
             ))
           ) : (
